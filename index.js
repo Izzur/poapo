b78f9be8884680172782aed1e81ee2a70db73cb6
Izzur Zuhri <izzur.zuhri@gmail.com>
2022-08-17 23:19:45 +0700

    Return jpeg composite of recent POAPs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require('fastify')()
 const axios = require('axios')
+const sharp = require('sharp')
 
 app.get('/:address', async (request, reply) => {
   const { address } = request.params
@@ -8,11 +9,31 @@ app.get('/:address', async (request, reply) => {
 
   const poapScan = 'https://frontend.poap.tech/actions/scan'
   const { data: poaps } = await axios.get(`${poapScan}/${address}`)
-  const eventImgs = poaps.map((p) => p?.event?.image_url)
-  return { data: eventImgs }
+  const imgUrls = poaps
+    .slice(0, 4)
+    .map((p) => axios.get(p?.event?.image_url, { responseType: 'arraybuffer' }))
+
+  const rawImgs = await Promise.all(imgUrls)
+  const toResize = rawImgs.map((i) => sharp(i.data).resize(150, 150).toBuffer())
+
+  const eventImgs = await Promise.all(toResize)
+
+  const compImg = await sharp({
+    create: {
+      width: eventImgs.length * 150,
+      height: 150,
+      channels: 3,
+      background: { r: 255, g: 255, b: 255 },
+    },
+  })
+    .composite(eventImgs.map((i, n) => ({ input: i, top: 0, left: n * 150 })))
+    .toFormat('jpeg')
+    .toBuffer({ resolveWithObject: true })
+
+  reply.type('image/jpeg').send(compImg.data)
 })
 
-app.get('/', async (request, reply) => {
+app.get('/', async (_request, _reply) => {
   return { status: 'OK' }
 })
 
